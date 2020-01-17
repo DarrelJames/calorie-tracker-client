7f3eb3a21eaff070dec9c7c7310998ab4e3c784c
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-16 23:52:56 -0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 import Registration from './containers/Registration'
+import LogIn from './containers/LogIn'
+import LogOut from './components/LogOut'
+import history from './history'
 
 function App() {
   return (
     <div className="ui container">
-      <Registration />
+      <Router history={history}>
+        <div>
+          <LogOut />
+          <Switch>
+            <Route path='/' exact component={LogIn} />
+            <Route path='/signup' component={Registration} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
