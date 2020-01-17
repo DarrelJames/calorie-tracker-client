3d8afc8d2996bb4911db37f67de7f4dc93a8463f
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-16 23:51:11 -0800

    Add login and logout reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { SIGN_UP } from '../actions/types'
+import { SIGN_UP, LOG_IN, LOG_OUT } from '../actions/types'
 
 const INITIAL_STATE = {
   isSignedIn: null,
@@ -9,7 +9,10 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_UP:
       return { ...state, isSignedIn: true, token: action.payload }
-      
+    case LOG_IN:
+      return { ...state, isSignedIn: true, token: action.payload }
+    case LOG_OUT:
+      return { ...state, isSignedIn: null, token: null }
     default:
       return state
   }
