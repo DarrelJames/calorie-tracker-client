61133c03b99831bc6a3f807a5ba309082559c204
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-16 23:52:33 -0800

    Add Login and Logout action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import api from '../apis/api'
 import {
   SIGN_UP,
   LOG_IN,
+  LOG_OUT,
   CREATE_ENTRY,
   UPDATE_ENTRY,
   DELETE_ENTRY,
@@ -13,4 +14,18 @@ export const signUp = formValues => async dispatch => {
   const token = response.headers.authorization.split(' ')[1]
 
   dispatch({ type: SIGN_UP, payload: token})
+}
+
+export const logIn = formValues => async dispatch => {
+  const response = await api.post('/login', {user: { ...formValues }})
+  const token = response.headers.authorization.split(' ')[1]
+
+  dispatch({ type: LOG_IN, payload: token})
+}
+
+export const logOut = () => async (dispatch, getState) => {
+  console.log(getState());
+  const response = await api.delete('/logout', {headers: { 'Authorization': `Bearer ${getState().auth.token}`}} )
+  
+  dispatch({ type: LOG_OUT})
 }
