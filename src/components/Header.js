2c52aaf61c2da3e3702dd83e5919031ad08a3c0a
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-19 08:19:26 -0800

    Add todays log and my account link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,21 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="ui secondary pointing menu">
-    <Link to="/" className="item">
-      Calorie Tracker
-    </Link>
+    <div className="ui massive secondary pointing menu">
+      <Link to="/" className="item">
+        Calorie Tracker
+      </Link>
+
+      <div className="right menu">
+        <Link to="/" className="item">
+          Today's Log
+        </Link>
+
+        <Link to='#' className='item'>
+          My Account
+        </Link>
+
+    </div>
 
 
     </div>
