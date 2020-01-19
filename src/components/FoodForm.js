67abdd7d3a548ca05933fc77d53c23919cabb5d1
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-19 00:13:22 -0800

    Create controlled form and handleSubmit function

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
+class FoodForm extends Component {
 
-export default class FoodForm extends Component {
+  state = {searchTerm: ''}
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.onSubmit(this.state.searchTerm)
+    this.setState({searchTerm: ''})
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: [e.target.value]
+    })
+  }
 
   render() {
     return (
-      <div>MyComponent</div>
-    );
+      <div className="ui category search" >
+
+      <form onSubmit={this.handleSubmit}>
+      <div className="ui icon input" >
+
+          <input
+            className='prompt'
+            name='searchTerm'
+            placeholder="Search foods..."
+            value={this.state.term}
+            onChange={this.handleChange}
+            onSubmit={this.handleSubmit}
+            >
+          </input>
+
+          <i className="search icon"> </i>
+        </div>
+        </form>
+
+      </div>
+    )
   }
 
 }
+export default (FoodForm)
