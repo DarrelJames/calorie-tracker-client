01fd6e807d373d339c600a2921903819393e3104
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-19 00:12:10 -0800

    add search food action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
 import api from '../apis/api'
+import edamam from '../apis/edamam'
 import {
   SIGN_UP,
   LOG_IN,
   LOG_OUT,
-  CREATE_ENTRY,
-  UPDATE_ENTRY,
-  DELETE_ENTRY,
-  FETCH_LOG
+  // CREATE_ENTRY,
+  // UPDATE_ENTRY,
+  // DELETE_ENTRY,
+  // FETCH_LOG,
+  SEARCH_FOOD
 } from './types'
 
 export const signUp = formValues => async dispatch => {
@@ -24,8 +26,21 @@ export const logIn = formValues => async dispatch => {
 }
 
 export const logOut = () => async (dispatch, getState) => {
-  console.log(getState());
-  const response = await api.delete('/logout', {headers: { 'Authorization': `Bearer ${getState().auth.token}`}} )
-  
+  await api.delete('/logout', {headers: { 'Authorization': `Bearer ${getState().auth.token}`}} )
+
   dispatch({ type: LOG_OUT})
+}
+
+export const searchFood = searchTerm => async (dispatch) => {
+
+  const response = await edamam.get('',{
+    params: {
+
+      'app_id': process.env.REACT_APP_EDAMAM_ID,
+      'app_key': process.env.REACT_APP_EDAMAM_KEY,
+      ingr: searchTerm[0]
+  }})
+
+
+  dispatch({ type: SEARCH_FOOD, payload: response.data.hints})
 }
