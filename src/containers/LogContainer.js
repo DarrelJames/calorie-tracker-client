94171793430ae093826fe8d3117ae11f388b5528
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-18 20:08:52 -0800

    Add Logcontainer

---
+++ src/containers/LogContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Log from '../components/Log'
+
+
+export default class LogContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        <Log />
+      </div>
+    );
+  }
+
+}
