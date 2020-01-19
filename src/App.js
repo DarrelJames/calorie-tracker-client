dc85e4f35485e6f170a44a12fe4f22f2b22d647a
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-19 00:11:39 -0800

    Remove foodform component and added foods container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import Registration from './containers/Registration'
 import LogIn from './containers/LogIn'
-import LogOut from './components/LogOut'
+// import LogOut from './components/LogOut'
 import history from './history'
 import Header from './components/Header'
 import LogContainer from './containers/LogContainer'
-import FoodForm from './components/FoodForm'
+import Foods from './containers/Foods'
 function App() {
   return (
     <div className="ui container">
@@ -18,7 +18,7 @@ function App() {
             <Route path='/' exact component={LogContainer} />
             <Route path='/login' component={LogIn} />
             <Route path='/signup' component={Registration} />
-            <Route path='/foods/new' component={FoodForm} />
+            <Route path='/foods/new' component={Foods} />
           </Switch>
         </>
       </Router>
