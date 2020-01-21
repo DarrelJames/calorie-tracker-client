cecf91d34508793c0b9c3525c7956eac0e57edaf
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-21 05:21:40 -0800

    Add logReducer and goalReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,14 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer'
 import edamamReducer from './edamamReducer'
+import logReducer from './logReducer'
+import goalReducer from './goalReducer'
+
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
-  edamam: edamamReducer
+  edamam: edamamReducer,
+  logs: logReducer,
+  goal: goalReducer
 });
