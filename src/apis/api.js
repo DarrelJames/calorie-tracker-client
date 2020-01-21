0882597bd497cf26ff9d901d94d99790cf9ccc19
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-21 05:20:37 -0800

    add default header authorization to axios instance

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-export default axios.create({
-  baseURL: 'http://localhost:3001',
-  headers: {
-    'Authorization': localStorage.getItem('token')
-  }
+const instance = axios.create({
+  baseURL: 'http://localhost:3001'
 })
+
+instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+
+export default instance
