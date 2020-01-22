de7cf9acc7bae29aeacd03f871c160b2e9812771
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-21 22:47:36 -0800

    add FETCH_LOGS SELECT_LOG types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,11 +1,17 @@
 export const SIGN_UP = 'SIGN_UP'
 export const LOG_IN = 'LOG_IN'
 export const LOG_OUT = 'LOG_OUT'
+
 export const CREATE_ENTRY = 'CREATE_ENTRY'
 export const UPDATE_ENTRY = 'UPDATE_ENTRY'
 export const DELETE_ENTRY = 'DELETE_ENTRY'
-export const FETCH_LOGS = 'FETCH_LOGS'
+
 export const SEARCH_FOOD = 'SEARCH_FOOD'
 export const SEARCHING_FOOD = 'SEARCHING_FOOD'
+
 export const FETCH_GOAL = 'FETCH_GOAL'
 export const SAVE_GOAL = 'SAVE_GOAL'
+
+
+export const FETCH_LOGS = 'FETCH_LOGS'
+export const SELECT_LOG = 'SELECT_LOG'
