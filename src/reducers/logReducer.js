89f1c7fae8ba1c0276eb259d501e44e6cb1043dd
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-21 22:44:37 -0800

    Add select_log reducer

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,13 +1,19 @@
-import { FETCH_LOGS } from '../actions/types'
+import { FETCH_LOGS, SELECT_LOG } from '../actions/types'
+import moment from 'moment'
 
 const INITIAL_STATE = {
-  
+  logs: [],
+  date: moment(),
+  current_log: 0,
+  selected_log: {}
 }
 
 export default (state = INITIAL_STATE, action ) => {
   switch (action.type) {
     case FETCH_LOGS:
       return { ...state , logs: action.payload}
+    case SELECT_LOG:
+      return { ...state, selected_log: action.payload}
 
     default:
       return state
