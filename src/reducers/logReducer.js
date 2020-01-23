089a52cbfae094505e23edd09c8940af7bdfd673
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-22 21:12:57 -0800

    Handle FETCH_LOG_SUCCESS and FETCH_LOG_START

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,23 +1,24 @@
-import { FETCH_LOGS, FETCH_LOG } from '../actions/types'
+import { FETCH_LOGS, FETCH_LOG_START, FETCH_LOG_SUCCESS } from '../actions/types'
 import _ from 'lodash'
 import moment from 'moment'
 
+
 const INITIAL_STATE = {
-  date: new Date()  
+  date: moment(new Date()).format('YYYY-MM-DD'),
+  fetchingLog: false,
+  logSet: false
 }
 
-const formatDate = date => {
-  return moment(date).format('MM-DD-YYYY')
-}
+
 
 export default (state = INITIAL_STATE, action ) => {
   switch (action.type) {
     case FETCH_LOGS:
       return { ...state , ..._.mapKeys(action.payload, 'date')}
-    case FETCH_LOG:
-      return { ...state, [action.payload.date]: action.payload}
-
-
+    case FETCH_LOG_START:
+      return { ...state, fetchingLog: true}
+    case FETCH_LOG_SUCCESS:
+      return { ...state, [action.payload.date]: action.payload,logSet: true, fetchingLog: false }
 
     default:
       return state
