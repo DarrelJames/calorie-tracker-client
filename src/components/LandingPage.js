272711f9f28c8de083a7844a90d1db9c1dc9db55
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-23 23:57:45 -0800

    Create landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Logo from '../images/kcal.png'
+
+export default class LandingPage extends Component {
+
+  render() {
+    return (
+      <h1 className='ui centered header'>
+        <img src={Logo} className='ui large image'/>
+        Calorie Tracker
+      </h1>
+    );
+  }
+
+}
