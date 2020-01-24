eecef0aa583fbbf9cd4fd8645577b15f8e6cb952
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-23 23:55:34 -0800

    Add Logo and styling to links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Logo from '../images/kcal.png'
 
 const Header = () => {
   return (
     <div className="ui massive secondary pointing menu">
-      <Link to="/" className="item">
+      <Link to="/" className="header item">
+        <img src={Logo} className='ui mini image' alt='logo'/>
         Calorie Tracker
+
       </Link>
 
       <div className="right menu">
-        <Link to="/logs" className="item">
+        <Link to="/logs" className="header item">
           Today's Log
         </Link>
 
-        <Link to='/profile' className='item'>
+        <Link to='/profile' className='header item'>
           My Account
         </Link>
 
