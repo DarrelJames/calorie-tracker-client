2b2800cbbfc0ad7e7921f39b7c9dd2c2af7acb26
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-24 19:21:35 -0800

    Add styling and messagebox

--- src/containers/Registration.js
+++ src/containers/Registration.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { signUp } from '../actions'
 import SignUp from '../components/SignUp'
-
+import '../css/fullscreenform.css'
+import RegistrationMessage from '../components/RegistrationMessage'
 class Registration extends Component {
 
   onSubmit = formValues => {
     this.props.signUp(formValues)
   }
-  
+
   render() {
     return (
-      <div>
-        <SignUp onSubmit={this.onSubmit}/>
+      <div className='ui middle aligned center aligned grid'>
+        <div className='column'>
+          <SignUp onSubmit={this.onSubmit}/>
+          <RegistrationMessage
+            message="Already have an account?"
+            link='/login'
+            linkText='Log In'
+          />
+        </div>
       </div>
-    );
+        );
   }
 
 }
