8e56c5d966f0872670160edd37ecec9232916cbe
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-24 21:11:00 -0800

    Added Error handling types for login and sign. Handle errors in reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,17 +1,36 @@
-import { SIGN_UP, LOG_IN, LOG_OUT } from '../actions/types'
+import {
+  SIGN_UP_START,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE,
+  LOG_IN_START,
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+  LOG_OUT
+} from '../actions/types'
 
 const INITIAL_STATE = {
   token: localStorage.getItem('token'),
-  error: '',
+  status: 'idle',
+  message: '',
   errorStatusCode: null,
 }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case SIGN_UP:
-      return { ...state, token: action.payload }
-    case LOG_IN:
-      return { ...state, token: action.payload }
+    case SIGN_UP_START:
+      return { ...state, status: 'loading' }
+    case SIGN_UP_SUCCESS:
+      return { ...state, ...INITIAL_STATE }
+    case SIGN_UP_FAILURE:
+      return { ...state, status: 'error', message: action.payload }
+
+    case LOG_IN_START:
+      return { ...state, status: 'loading'}
+    case LOG_IN_SUCCESS:
+      return { ...state, ...INITIAL_STATE }
+    case LOG_IN_FAILURE:
+      return { ...state, status: 'error', message: action.payload.data, errorStatusCode: action.payload.status }
+
     case LOG_OUT:
       return { ...state, token: null }
     default:
