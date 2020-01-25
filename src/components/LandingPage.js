65c71cc0116b66cf35e633a13e634818b2b130ef
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-25 09:33:19 -0800

    Redirect to login to logs for now

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import Logo from '../images/kcal.png'
-
-export default class LandingPage extends Component {
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'
+class LandingPage extends Component {
 
   render() {
+
     return (
-      <h1 className='ui centered header'>
-        <img src={Logo} className='ui large image' alt='logo'/>
-        Calorie Tracker
-      </h1>
+      <>
+        {this.props.token ? (
+          <Redirect to="/logs" />
+        ) : (
+          <Redirect to="/login" />
+        )}
+        <h1 className='ui centered header'>
+          <img src={Logo} className='ui large image' alt='logo'/>
+          Calorie Tracker
+        </h1>
+        
+        <div className="ui placeholder segment" >
+          <div className="ui two column very relaxed stackable grid" >
+            <div className="column">
+
+              <div className="ui blue submit button">Login</div>
+
+            </div>
+            <div className="middle aligned column">
+              <div className="ui big button">
+                <i className="signup icon"></i>
+                Sign Up
+              </div>
+            </div>
+          </div>
+          <div className="ui vertical divider">
+            Or
+          </div>
+        </div>
+      </>
     );
   }
 
 }
+const mapStateToProps = ({ auth: { token }}) => ({token})
+
+export default connect(mapStateToProps)(LandingPage)
