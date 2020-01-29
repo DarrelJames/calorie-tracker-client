d37568e539f5ead5ff1bcfd324f0fa4fc97a6a7a
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-28 18:49:34 -0800

    Calculate totals and render MealTotalsRow component

--- src/containers/MealGroup.js
+++ src/containers/MealGroup.js
@@ -3,20 +3,40 @@ import { Link } from 'react-router-dom'
 import Food from '../components/Food'
 import { connect } from 'react-redux'
 import { Table, Icon, Transition } from 'semantic-ui-react'
+import MealTotalsRow from '../components/MealTotalsRow'
 
 class MealGroup extends Component {
 
   renderFoods = () => {
-
     if ( this.props.logSet){
-      const entries = this.props.log.entries.filter(entry => entry.category === this.props.category)
-      return entries.map(entry => entry.foods.map((food) => {
+      return this.props.entries.map(entry => entry.foods.map((food) => {
         return <Transition.Group animation='slide down' duration='500' as={Food} servings={entry.servings} entry_id={entry.id} key={food.id} {...food} />
       }
       ))
     }
   }
+
+  getTotals = () => {
+    if ( this.props.logSet) {
+      let calories, carbs, fat, protein
+      calories = carbs = fat = protein = 0
+      this.props.entries.forEach(entry => entry.foods.forEach(item => {
+        calories += item.calories * entry.servings;
+        carbs += item.carbs * entry.servings;
+        fat += item.fat * entry.servings;
+        protein += item.protein * entry.servings;
+      }))
+      return {
+        calories,
+        carbs,
+        fat,
+        protein
+      }
+    }
+  }
+
   render(){
+    const mealTotals = this.getTotals()
     return (
 
       <Table structured unstackable selectable>
@@ -30,25 +50,31 @@ class MealGroup extends Component {
           </Table.Row>
         </Table.Header>
         <Table.Body>
-
           {this.renderFoods()}
-        </Table.Body>
-        <Table.Footer>
-          <Table.Row>
+          <Table.Row colSpan={5}>
             <Table.Cell>
               <Icon color='green' name='plus'/>
               <Link to={`/entries/new?category=${this.props.category}`}>Add Food Item</Link>
             </Table.Cell>
           </Table.Row>
-        </Table.Footer>
+        </Table.Body>
+
+        {(this.props.logSet && this.props.entries.length > 0) &&  <MealTotalsRow {...mealTotals}/>}
+        
       </Table>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
+  const cat = ownProps.category
+  const log = state.logs[state.logs.date]
+  let entries = null
+  if (log) {
+    entries = log[cat]
+  }
 
-  return { log: state.logs[state.logs.date], logSet: state.logs.logSet}
+  return { entries, logSet: state.logs.logSet}
 }
 
 export default connect(mapStateToProps)(MealGroup);
